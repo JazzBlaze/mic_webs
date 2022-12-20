87b28c56dcca1131a0fe18efbc95d46422b7c85a
JazzBlaze <thejasdub@gmail.com>
2022-12-20 23:28:14 +0530

    initialized basic files and boiler code.

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+function navbar() {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to="/" className="navbar-logo">
+                    MIC
+                </Link>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default navbar
