5db6deb8f3d8a4c4da04b98a53e351f0a4fc5ef8
ItsTulasiRaman <tulasiraman917@gmail.com>
2022-12-21 22:36:42 +0530

    Pushed VitCards

--- src/components/VitCards.js
+++ src/components/VitCards.js
@@ -1,4 +1,4 @@
-
+import VitLogo from "../images/vit_logo.png";
 
 const VitCards = () => {
     return (
@@ -25,15 +25,15 @@ const VitCards = () => {
              </div>
 		</div>
 	</div> */}
-    <section id="VitCards">
-            <div className="flex justify-center h-[100vh]">
-                <div className="relative flex flex-col items-center gap-3 space-y-15 md:space-x-15 p-[3%] border-[20px] border-grey w-[100%]">
+    <section id="VitCards" className="h-[100vh]">
+             <div className="flex justify-center py-[10%]">
+                <div className="relative flex flex-col items-center gap-3 space-y-15 md:space-x-15 p-[3%] border-[20px] border-red">
                     <div className="flex justify-center">
-                        <div className="relative flex flex-col items-center gap-3 rounded-[30px] space-y-15 md:space-x-15 shadow-lg p-[3%] w-[100%] md:max-w-[100%] border-[20px] border-grey">
+                        <div className="relative flex flex-col items-center rounded-[30px] space-y-15 md:space-x-15 shadow-lg p-[3%] w-[100%] md:max-w-[100%] border-[20px] border-grey">
                             <div>
                                 <h3 className="font-black text-gray-800 md:text-3xl text-xl">VIT. It says it all.</h3>
                             </div>
-                            <div className="flex flex-col md:flex-row md:space-x-6 space-y-8 md:space-y-0">
+                            <div className="flex flex-col md:flex-row md:space-x-1 space-y-8 md:space-y-0">
                                 <div className="p-[2%]">
                                     <p className="md:text-lg text-gray-500 text-base">One of India's top engineering schools,
                                         Vellore Institute of Technology equips students with the best exposure
@@ -43,7 +43,7 @@ const VitCards = () => {
 
                                 <div className="md:w-1/3 flex justify-between item-center">
                                     <div>
-                                        <img className="" src={"/images/vit_logo.png"} alt="VIT LOGO" /></div>
+                                        <img className="px-[40px]" src={VitLogo} alt="VIT LOGO" /></div>
                                 </div>
                             </div>
 
@@ -53,7 +53,7 @@ const VitCards = () => {
 
                     <div>
                         <div className="flex justify-center">
-                            <div className="relative flex flex-col gap-3 space-y-15 md:space-x-15 w-[100%] md:max-w-[100%] mx-auto border border-white">
+                            <div className="relative flex flex-col gap-3 space-y-15 md:space-x-15 w-[100%] md:max-w-[100%] mx-auto border-[20px] border-grey">
                                 <div className="flex flex-col md:flex-row md:space-x-6 space-y-8 md:space-y-0 w-[100%]">
                                     <div className="p-[2%] shadow-lg rounded-[30px]">
                                         <div>
@@ -63,7 +63,7 @@ const VitCards = () => {
                                         </div>
                                     </div>
 
-                                    <div className="p-[4%]  md:w-1/3 flex shadow-lg justify-between item-center  rounded-[30px]">
+                                    <div className="p-[4%]  md:w-1/3 flex shadow-lg justify-between item-center rounded-[30px]">
                                         <div>
                                             <h3 className="font-black text-gray-800 md:text-3xl text-xl">Proactive</h3>
 
