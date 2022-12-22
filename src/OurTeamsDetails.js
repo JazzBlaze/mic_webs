b287e40bdc027fd5be214ffa1c99817d63e48317
ItsTulasiRaman <tulasiraman917@gmail.com>
2022-12-23 01:26:10 +0530

    Wrapped up OurTeams page

---
+++ src/OurTeamsDetails.js
@@ -0,0 +1,54 @@
+const OurTeamDetails = {
+    president:[
+        {
+        Name:"Sam Prince Franklin",
+        image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+        quote:"I'm Sam",
+        linkedIn:"https://www.linkedin.com/in/samprincefranklin/"
+    },
+    ],
+    heads:[
+        {
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },
+        {
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },
+        {
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },{
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },{
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },
+        {
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        },
+        {
+            Name:"Head",
+            image_url: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+            quote:"Me head",
+            linkedIn:"https://www.linkedin.com/in/samprincefranklin/" 
+        }
+    ]
+}
+
+export default OurTeamDetails;
---
+++ src/components/OurTeams.jsx
@@ -0,0 +1,66 @@
+import OurTeamDetails from "../OurTeamsDetails";
+
+const OurTeam = () => {
+    return (
+        <>
+            <section id="OurTeams" className="h-[140vh]">
+                <div>
+                    <h2 className="flex justify-center">Our Team</h2>
+                    <div className="flex flex-row justify-center w-full mt-[50px] mx-[10px] px-[10%]">
+
+                        <div className="flex flex-row justify-center w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]">
+                            <div className="w-[35%]">
+                                <img className="w-[100%] rounded-[30px]" src={OurTeamDetails.president[0].image_url} alt="President pic" onClick={() => (window.open(OurTeamDetails.president[0].linkedIn, '_blank'))} />
+                            </div>
+                            <div className="w-[70%]">
+                                <h4>{OurTeamDetails.president[0].Name}</h4>
+                                <p className="pt-4">
+                                    {OurTeamDetails.president[0].quote}
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                        <div className="flex justify-center">
+                            <div className="flex flex-row justify-center flex-wrap w-full mt-[30px] mx-[50px] px-[15%] border">
+                                {OurTeamDetails.heads.map((item, index) => (
+                                    <OurTeamCard key={index} {...item} />
+                                ))
+                                }
+                            </div>
+                        </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default OurTeam;
+
+
+const OurTeamCard = ({ Name, image_url, quote, linkedIn }) => {
+    return (<>
+        <div className="flex justify-center mt-[50px] mx-[20px]">
+
+            <div className="flex flex-row justify-center w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]">
+                            <div className="w-[35%]">
+                                <img className="w-[100%] rounded-[30px]" src={image_url} alt="President pic" onClick={() => (window.open(linkedIn, '_blank'))} />
+                            </div>
+                            <div className="w-[70%]">
+                                <h4>{Name}</h4>
+                                <p className="pt-4">
+                                    {quote}
+                                </p>
+                            </div>
+                        </div>
+
+        </div>
+    </>);
+};
+
+
+
+
+
