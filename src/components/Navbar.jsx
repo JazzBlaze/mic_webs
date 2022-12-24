63966959549138f34b7d55740401ff93b19b24e7
JazzBlaze <thejasdub@gmail.com>
2022-12-24 22:46:55 +0530

    updated logo.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
                     </Link>
                   </li>
                   <li className='nav-item'>
-                    <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/VitCards' className='nav-links' onClick={closeMobileMenu}>
                       About
                     </Link>
                   </li>
