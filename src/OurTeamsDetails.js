64e740b03a38957f0c7da001eb2e52f448a077fd
ItsTulasiRaman <tulasiraman917@gmail.com>
2022-12-26 11:50:09 +0530

    Fixed details

--- src/OurTeamsDetails.js
+++ src/OurTeamsDetails.js
@@ -1,12 +1,19 @@
 const OurTeamDetails = {
     faculty:[
         {
+            Name:"Dr. Braveen M",
+            image_url:"https://res.cloudinary.com/dsolrmyfj/image/upload/v1672034970/WhatsApp_Image_2022-12-26_at_11.37.58_AM_ge4hkc.jpg",
+            role:"Associate Professor",
+            quote:"Computer Science professor at VIT Chennai. PhD from VIT Vellore in network security. She loves preparing club members for their futures.",
+            linkedIn:"",
+        },
+        {
             Name:"Dr. Anusha Kannan",
             image_url:"https://res.cloudinary.com/dsolrmyfj/image/upload/v1671513174/Fac_srhdnx.png",
             role:"Associate Professor",
             quote:"Computer Science professor at VIT Chennai. PhD from VIT Vellore in network security. She loves preparing club members for their futures.",
             linkedIn:"",
-        }
+        },
     ],
     president:[
         {
@@ -56,6 +63,132 @@ const OurTeamDetails = {
             linkedIn:"https://www.linkedin.com/in/neeraj-j-manurkar-64372b212/" 
         },
         {
+            Name:"Shyam Sundhar G",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671512617/shyam_g7ze3n.jpg",
+            role:"Dev lead",
+            quote:"Work Hard Anywhere",
+            linkedIn:"https://www.linkedin.com/in/shyamsundhar2435/" 
+        },
+        {
+            Name:"Samarth Asthan",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/52A1F80B-C262-4FE2-8390-32AD787D714B_-_Samarth_Asthan_21BRS1248_i96emj.jpg",
+            role:"App Development head",
+            quote:"Progressive, Quick Learner and Versatile in New Technologies",
+            linkedIn:"https://www.linkedin.com/in/samarthasthan/" 
+        },
+        {
+            Name:"Aravind P",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543609/aravindp_gqnn3m.jpg",
+            role:"Web dev lead",
+            quote:"Proficient Professional Pragmatic Pythonic Programmer",
+            linkedIn:"https://www.linkedin.com/in/aravind-p-92a5061b8/" 
+        },
+        {
+            Name:"Kiranraaj Sekar",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543619/kiran_dzvwxp.jpg",
+            role:"Web dev lead",
+            quote:"Linux enthusiast with unhealthy love for rust",
+            linkedIn:"https://www.linkedin.com/in/kiranraaj-sekar-a950081b9/" 
+        },
+        {
+            Name:"Kaifur Rahman",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG_1976_-_Kaifur_Rahman_21BRS1147_anhtdh.jpg",
+            role:"Web dev head",
+            quote:"Positive mindset and enjoys challenges because hardwork never bothers me.",
+            linkedIn:"https://www.linkedin.com/in/kaifur-rahman" 
+        },
+        {
+            Name:"Akilesh S",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/8817_-_Copy.JPG_-_Akilesh_S_21MIS1167_pn1phu.jpg",
+            role:"Web Dev head",
+            quote:"Making the World a Better Place ⚡",
+            linkedIn:"https://www.linkedin.com/in/akilesh-s-5b4b8374/" 
+        },
+        {
+            Name:"Gokulrupachandran",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/1666193670604_-_RUPACHANDRAN_20MIA1132_w1wtbl.jpg",
+            role:"Cyber Security lead",
+            quote:"Security is a Myth",
+            linkedIn:"https://www.linkedin.com/in/rupachandrans/" 
+        },
+        {
+            Name:"Muhammed Riyaz S",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/270207219_718026219602318_4536025956078904265_n_-_Muhammed_Riyaz_S_21BPS1147_gckzts.jpg",
+            role:"Cyber security head",
+            quote:"El Que No Arriesga, No Gana",
+            linkedIn:"https://www.linkedin.com/in/muhammedriyazs" 
+        },
+        {
+            Name:"Nalin Iyer",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/IMG-20220610-WA0017_-_Nalin_Iyer_21BRS1631_tqnrdf.jpg",
+            role:"Cybersecurity, Technical head",
+            quote:"Trust no one, verify everything.",
+            linkedIn:"https://www.linkedin.com/in/nalin-iyer" 
+        },
+        {
+            Name:"Marian George",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/id_pic_-_Marian_George_21BAI1726_uq2rhr.jpg",
+            role:"Cyber Security head",
+            quote:"Nothing is apparent.",
+            linkedIn:"https://www.linkedin.com/in/marian-george-718bb6219" 
+        },
+        {
+            Name:"Aneesh Aparajit G",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978206/IMG-20221206-WA0014_-_ANEESH_APARAJIT_G_20BRS1054_agfgkk.jpg",
+            role:"AI/ML lead",
+            quote:"Bhaad mein jaa... ",
+            linkedIn:"https://www.linkedin.com/in/aneesh-aparajit" 
+        },
+        {
+            Name:"Sanjay Saju Jacob",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG20220811115534_-_SANJAY_SAJU_JACOB_20BAI1139_ytqijs.jpg",
+            role:"Competitive Programming lead",
+            quote:"I mod U 3000",
+            linkedIn:"https://www.linkedin.com/in/sanjay-saju-jacob-ab738a1bb/" 
+        },
+        {
+            Name:"Appana Jaya Ratna Uttam ",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/IMG_20221224_112135_-_Appana_Jaya_Ratna_Uttam_21BCE5766_chdvi5.jpg",
+            role:"Competitive Coding head",
+            quote:"All human wisdom is summed up in two words; patience and hope!",
+            linkedIn:"https://www.linkedin.com/in/jayaratnauttam-appana-148562239" 
+        },
+        {
+            Name:"P.Je Sai Kailash",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543632/Prhead_lhcdes.jpg",
+            role:"Management lead",
+            quote:"Having the dream is easy, making it come true is hard",
+            linkedIn:"https://www.linkedin.com/in/je-sai-kailash-pulipati-347180202/" 
+        },
+        {
+            Name:"Ramireddy Jeevan Reddy",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/05899605-2188-409A-AE2E-30C784306A9F_-_Ramireddy_Jeevan_Reddy_21BCE5163_r8n5tg.jpg",
+            role:"Management head",
+            quote:"Jeevan Reddy",
+            linkedIn:"https://www.linkedin.com/mwlite/in/ramireddy-jeevan-reddy-731a4b233" 
+        },
+        {
+            Name:"Varshith R P",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/IMG_20220810_131231_Bokeh_polarr_2_-_Varshith_R_P_21BRS1366_fju4e0.jpg",
+            role:"Marketing and PR head",
+            quote:"Gregarious!",
+            linkedIn:"https://www.linkedin.com/in/varshith-r-p-82ba15234" 
+        },
+        {
+            Name:"Shashannk S",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/MIC_Shashannk_-_Shashannk_S_21BRS1241_kau5n5.jpg",
+            role:"Marketing & PR head",
+            quote:"My existence is a scandal for the antagonist",
+            linkedIn:"http://www.linkedin.com/in/shashannk-s-316a06236" 
+        },
+        {
+            Name:"K Ravi Teja ",
+            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG20221218141038_-_Kilaparthi_Ravi_Teja_21BCE5627_gxzhyf.jpg",
+            role:"Marketing and PR head",
+            quote:"With the new day comes new strength and new thoughts",
+            linkedIn:"https://www.linkedin.com/in/ravi-teja-1b70b2250" 
+        },
+        {
             Name:"Muskaan Yadav",
             image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978209/Muskaan-pic_-_MUSKAAN_YADAV_20BCE1622_ceew0c.jpg",
             role:"Design and Content lead",
@@ -91,128 +224,9 @@ const OurTeamDetails = {
             linkedIn:"https://www.linkedin.com/in/anadi-goswami-6326b3234" 
         },
         {
-            Name:"P.Je Sai Kailash",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543632/Prhead_lhcdes.jpg",
-            role:"Marketing and Public Relation lead",
-            quote:"Having the dream is easy, making it come true is hard",
-            linkedIn:"https://www.linkedin.com/in/je-sai-kailash-pulipati-347180202/" 
-        },
-        {
-            Name:"Varshith R P",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/IMG_20220810_131231_Bokeh_polarr_2_-_Varshith_R_P_21BRS1366_fju4e0.jpg",
-            role:"Marketing and PR head",
-            quote:"Gregarious!",
-            linkedIn:"https://www.linkedin.com/in/varshith-r-p-82ba15234" 
-        },
-        {
-            Name:"Shashannk S",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/MIC_Shashannk_-_Shashannk_S_21BRS1241_kau5n5.jpg",
-            role:"Marketing & PR head",
-            quote:"My existence is a scandal for the antagonist",
-            linkedIn:"http://www.linkedin.com/in/shashannk-s-316a06236" 
-        },
-        {
-            Name:"K Ravi Teja ",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG20221218141038_-_Kilaparthi_Ravi_Teja_21BCE5627_gxzhyf.jpg",
-            role:"Marketing and PR head",
-            quote:"With the new day comes new strength and new thoughts",
-            linkedIn:"https://www.linkedin.com/in/ravi-teja-1b70b2250" 
-        },
-        {
-            Name:"Aravind P",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543609/aravindp_gqnn3m.jpg",
-            role:"Web dev lead",
-            quote:"Proficient Professional Pragmatic Pythonic Programmer",
-            linkedIn:"https://www.linkedin.com/in/aravind-p-92a5061b8/" 
-        },
-        {
-            Name:"Kiranraaj Sekar",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671543619/kiran_dzvwxp.jpg",
-            role:"Web dev lead",
-            quote:"Linux enthusiast with unhealthy love for rust",
-            linkedIn:"https://www.linkedin.com/in/kiranraaj-sekar-a950081b9/" 
-        },
-        {
-            Name:"Kaifur Rahman",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG_1976_-_Kaifur_Rahman_21BRS1147_anhtdh.jpg",
-            role:"Web dev head",
-            quote:"Positive mindset and enjoys challenges because hardwork never bothers me.",
-            linkedIn:"https://www.linkedin.com/in/kaifur-rahman" 
-        },
-        {
-            Name:"Akilesh S",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/8817_-_Copy.JPG_-_Akilesh_S_21MIS1167_pn1phu.jpg",
-            role:"Web Dev",
-            quote:"Making the World a Better Place ⚡",
-            linkedIn:"https://www.linkedin.com/in/akilesh-s-5b4b8374/" 
-        },
-        {
-            Name:"Shyam Sundhar G",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671512617/shyam_g7ze3n.jpg",
-            role:"Dev lead",
-            quote:"Work Hard Anywhere",
-            linkedIn:"https://www.linkedin.com/in/shyamsundhar2435/" 
-        },
-        {
-            Name:"Samarth Asthan",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/52A1F80B-C262-4FE2-8390-32AD787D714B_-_Samarth_Asthan_21BRS1248_i96emj.jpg",
-            role:"App Development head",
-            quote:"Progressive, Quick Learner and Versatile in New Technologies",
-            linkedIn:"https://www.linkedin.com/in/samarthasthan/" 
-        },
-        {
-            Name:"Aneesh Aparajit G",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978206/IMG-20221206-WA0014_-_ANEESH_APARAJIT_G_20BRS1054_agfgkk.jpg",
-            role:"AI/ML lead",
-            quote:"Bhaad mein jaa... ",
-            linkedIn:"https://www.linkedin.com/in/aneesh-aparajit" 
-        },
-        {
-            Name:"Gokulrupachandran",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/1666193670604_-_RUPACHANDRAN_20MIA1132_w1wtbl.jpg",
-            role:"Cyber Security lead",
-            quote:"Security is a Myth",
-            linkedIn:"https://www.linkedin.com/in/rupachandrans/" 
-        },
-        {
-            Name:"Muhammed Riyaz S",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/270207219_718026219602318_4536025956078904265_n_-_Muhammed_Riyaz_S_21BPS1147_gckzts.jpg",
-            role:"Cyber security head",
-            quote:"El Que No Arriesga, No Gana",
-            linkedIn:"https://www.linkedin.com/in/muhammedriyazs" 
-        },
-        {
-            Name:"Nalin Iyer",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/IMG-20220610-WA0017_-_Nalin_Iyer_21BRS1631_tqnrdf.jpg",
-            role:"Cybersecurity, Technical",
-            quote:"Trust no one, verify everything.",
-            linkedIn:"https://www.linkedin.com/in/nalin-iyer" 
-        },
-        {
-            Name:"Marian George",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978205/id_pic_-_Marian_George_21BAI1726_uq2rhr.jpg",
-            role:"Cyber Security",
-            quote:"Nothing is apparent.",
-            linkedIn:"https://www.linkedin.com/in/marian-george-718bb6219" 
-        },
-        {
-            Name:"Sanjay Saju Jacob",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978207/IMG20220811115534_-_SANJAY_SAJU_JACOB_20BAI1139_ytqijs.jpg",
-            role:"Competitive Programming lead",
-            quote:"I mod U 3000",
-            linkedIn:"https://www.linkedin.com/in/sanjay-saju-jacob-ab738a1bb/" 
-        },
-        {
-            Name:"Appana Jaya Ratna Uttam ",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978208/IMG_20221224_112135_-_Appana_Jaya_Ratna_Uttam_21BCE5766_chdvi5.jpg",
-            role:"Competitive Coding head",
-            quote:"All human wisdom is summed up in two words; patience and hope!",
-            linkedIn:"https://www.linkedin.com/in/jayaratnauttam-appana-148562239" 
-        },
-        {
             Name:"Sahil Sharma",
             image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978209/Screenshot_2022_0925_212931_-_Sahil_Sharma_21BLC1414_aavgjj.jpg",
-            role:"Photography",
+            role:"Photography head",
             quote:"Achha thik hai samajh gaya",
             linkedIn:"https://www.linkedin.com/in/sahil-sharma-885b04225" 
         },
@@ -222,13 +236,6 @@ const OurTeamDetails = {
             role:"Sponsorship Lead",
             quote:"Quality never goes out of style",
             linkedIn:"http://www.linkedin.com/in/tejas-krishna-886812224" 
-        },
-        {
-            Name:"Ramireddy Jeevan Reddy",
-            image_url: "https://res.cloudinary.com/dsolrmyfj/image/upload/v1671978204/05899605-2188-409A-AE2E-30C784306A9F_-_Ramireddy_Jeevan_Reddy_21BCE5163_r8n5tg.jpg",
-            role:"Management",
-            quote:"Jeevan Reddy",
-            linkedIn:"https://www.linkedin.com/mwlite/in/ramireddy-jeevan-reddy-731a4b233" 
         },
     ]
 }
--- src/components/OurTeams.jsx
+++ src/components/OurTeams.jsx
@@ -6,9 +6,10 @@ const OurTeam = () => {
             <section id="OurTeams" className="bg-white">
                 <div>
                     <h2 className="flex justify-center m-[30px]">Our Team</h2>
-                    <div className="flex flex-row justify-center w-full mt-[30px] mx-[10px] px-[10%] gap-10">
+                    <div className="flex flex-row flex-wrap justify-center w-full mt-[30px] mx-[10px] px-[10%] gap-10">
+                        
                         <div className="hover:-translate-y-1 hover:scale-110 duration-300 flex justify-center min-w-[350px] w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]" onClick={() => (window.open(OurTeamDetails.secretary[0].linkedIn, '_blank'))} >
-                            <div className="w-[35%]">
+                            <div className="w-[35%] flex self-center">
                                 <img className="w-[100%] rounded-full" src={OurTeamDetails.faculty[0].image_url} alt="Associate proffesor" />
                             </div>
                             <div className="w-[70%]">
@@ -19,12 +20,26 @@ const OurTeam = () => {
                                 </p>
                             </div>
                         </div>
+
+                        <div className="hover:-translate-y-1 hover:scale-110 duration-300 flex justify-center min-w-[350px] w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]" onClick={() => (window.open(OurTeamDetails.secretary[0].linkedIn, '_blank'))} >
+                            <div className="w-[35%] flex self-center">
+                                <img className="w-[100%] rounded-full" src={OurTeamDetails.faculty[1].image_url} alt="Associate proffesor" />
+                            </div>
+                            <div className="w-[70%]">
+                                <h4>{OurTeamDetails.faculty[1].Name}</h4>
+                                <p className="text-[15px] font-semibold">{OurTeamDetails.faculty[1].role}</p>
+                                <p className="pt-4 text-[15px]">
+                                    {OurTeamDetails.faculty[1].quote}
+                                </p>
+                            </div>
+                        </div>
+
                     </div>
 
                     <div className="flex flex-row flex-wrap justify-center w-full mt-[30px] mx-[10px] px-[10%] gap-10">
 
                         <div className="hover:-translate-y-1 hover:scale-110 duration-300 flex justify-center min-w-[350px] w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]" onClick={() => (window.open(OurTeamDetails.president[0].linkedIn, '_blank'))}>
-                            <div className="w-[35%]">
+                            <div className="w-[35%] flex self-center">
                                 <img className="w-[100%] rounded-full" src={OurTeamDetails.president[0].image_url} alt="President pic" />
                             </div>
                             <div className="w-[70%]">
@@ -37,7 +52,7 @@ const OurTeam = () => {
                         </div>
 
                         <div className="hover:-translate-y-1 hover:scale-110 duration-300 flex flex-row justify-center min-w-[350px] w-[350px] gap-8 rounded-[30px] p-[1%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]" onClick={() => (window.open(OurTeamDetails.secretary[0].linkedIn, '_blank'))} >
-                            <div className="w-[35%]">
+                            <div className="w-[35%] flex self-center">
                                 <img className="w-[100%] rounded-full" src={OurTeamDetails.vice_president[0].image_url} alt="Associate proffesor" />
                             </div>
                             <div className="w-[70%]">
@@ -74,7 +89,7 @@ const OurTeamCard = ({ Name, image_url, role, quote, linkedIn }) => {
         <div className="flex justify-center mt-[30px] mx-[10px]">
 
             <div className="hover:-translate-y-1 hover:scale-110 duration-300 flex flex-row justify-center w-[350px] gap-8 rounded-[30px] p-[4%] shadow-[0_3px_20px_0px_rgba(0,0,0,0.2)]" onClick={() => (window.open(linkedIn, '_blank'))} >
-                <div className="w-[35%]">
+                <div className="w-[35%] flex self-center">
                     <img className="w-[100%] rounded-full aspect-square" src={image_url} alt="MIC department heads pic" />
                 </div>
                 <div className="w-[70%]">
